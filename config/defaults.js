723b16e43a0bfc460d44db056869e68d74fa0768
RaulJimenezOrtega <hhkaos@gmail.com>
2016-09-11 17:52:06 +0200

    Updating oauthappid

--- config/defaults.js
+++ config/defaults.js
@@ -20,7 +20,7 @@ define({
   //default web map, default app color theme and more. These values can be overwritten by template configuration settings and url parameters.
   "appid": "",
   "webmap":  "589a3ac57b7844618f9a8ee94ca49d59",
-  "oauthappid": "IG9ghsNE4g47w4XH",
+  "oauthappid": "d17Yd7ULGJ2yT9e4",
   //Group templates must support a group url parameter. This will contain the id of the group.
   "group": "",
   //Enter the url to the proxy if needed by the application. See the 'Using the proxy page' help topic for details
